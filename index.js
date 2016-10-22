c106c1b39ae1ac6689e4533b6ca753eb7d06211b
shekit <as7790@nyu.edu>
2016-10-22 17:29:52 -0400

    end of session node js

--- index.js
+++ index.js
@@ -1,18 +1,39 @@
 var express = require('express')
+var path = require('path')
 
 var app = express()
 
 var http = require('http')
 http = http.Server(app)
 
+// MIDDLEWARE
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine','pug')
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+
 app.get('/', function(req, res){
 
-	res.send("Hello ITP")
+	var name = "Abhishek"
+
+	// get name from database
+
+	// add first name and last name
+
+	// set it to a variable
+
+	// pass that vairable to the template
+
+	res.render("index.pug", {myname:name})
 })
 
 app.get('/about', function(req, res){
 
-	res.send("ABOUT ITP")
+	var list = ["chris","leslie","paula","jenny","ondina","patrick"]
+	var meeting = false;
+
+	res.render("about.pug", {namelist: list, is_meeting_on: meeting})
 })
 
 app.get('/contact', function(req, res){
